605e58b1f09a8481f0abddec296699c09e8b9478
Gavin Behel <behelgavin138@gmail.com>
2019-05-22 12:33:19 -0500

    update

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,13 +1,13 @@
-public class Final
+public class SlotMachine
 {
     public static void main(String[] args){
-        String result = null;
+        String result ="You win";
         int random = (int) (Math.random() * 10);
         int random2 = (int) (Math.random() * 10);
         int random3 = (int) (Math.random() * 10);
 
         if( random == random2 && random2 == random3 ){
-            result = "You Win";
+            result = "You win";
         }
         if( random != random2 && random2 != random3) {
             result = "You lose";
