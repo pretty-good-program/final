9a07169007af53e4da42e9055dd536c7277edda1
Chance Niffen <ozzyniffen@gmail.com>
2019-06-04 11:56:46 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,2 @@
 public class Main {
-
-    public static void main(String[] args){
-
-        System.out.println("Welcome to the casino");
-        System.out.println("What game would you like to play?");
-        BlackJack.runBlackJack();
-
-
-    }
-
 }
